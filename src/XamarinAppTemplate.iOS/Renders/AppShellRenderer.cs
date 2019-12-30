0937ce2457a0b9f9dadb13044060c48dda502f2f
Julien Bisconti <julienbi@unity3d.com>
2019-12-30 04:57:31 +0300

    whoa

--- src/XamarinAppTemplate.iOS/Renders/AppShellRenderer.cs
+++ src/XamarinAppTemplate.iOS/Renders/AppShellRenderer.cs
@@ -17,22 +17,49 @@ namespace XamarinAppTemplate.iOS.Renders
 
         }
 
-        protected override IShellFlyoutContentRenderer CreateShellFlyoutContentRenderer()
+        protected override IShellFlyoutRenderer CreateFlyoutRenderer()
         {
-            var result = base.CreateShellFlyoutContentRenderer();
-            var view = result.ViewController.View;
-
-            view.SemanticContentAttribute = UISemanticContentAttribute.ForceRightToLeft;
-            view.SetNeedsLayout();
-
-            return result;
+            return new ShellFlyoutRenderer()
+            {
+                FlyoutTransition = new CustomTrans()
+            };
         }
 
+        
         protected override IShellItemTransition CreateShellItemTransition()
         {
             var result = base.CreateShellItemTransition();
 
             return result;
+        }
+    }
+
+    public class CustomTrans : IShellFlyoutTransition
+    {
+        public void LayoutViews(CGRect bounds, nfloat openPercent, UIView flyout, UIView shell, FlyoutBehavior behavior)
+        {
+            if (behavior == FlyoutBehavior.Locked)
+                openPercent = 1;
+
+            nfloat flyoutWidth = (nfloat)(Math.Min(bounds.Width, bounds.Height) * 0.8);
+            nfloat openLimit = flyoutWidth;
+            nfloat openPixels = openLimit * openPercent;
+
+            if (behavior == FlyoutBehavior.Locked)
+                shell.Frame = new CGRect(bounds.X + flyoutWidth, bounds.Y, bounds.Width - flyoutWidth, bounds.Height);
+            else
+                shell.Frame = bounds;
+
+            //flyout.Frame = new CGRect(-openLimit + openPixels, 0, flyoutWidth, bounds.Height);
+
+            flyout.Frame = new CGRect(0, 0, flyoutWidth, bounds.Height);
+
+
+            if (behavior != FlyoutBehavior.Locked)
+            {
+                var shellOpacity = (nfloat)(0.5 + (0.5 * (1 - openPercent)));
+                shell.Layer.Opacity = (float)shellOpacity;
+            }
         }
     }
 
