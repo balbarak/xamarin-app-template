df7efae54f3e2e903ed8cc9f0154aa791e33e772
balbarak <balbarak@live.com>
2020-01-04 17:33:00 +0300

    update

---
+++ src/XamarinAppTemplate/Helpers/SvgToImageSource.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+using Xamarin.Forms.Svg;
+
+namespace XamarinAppTemplate
+{
+    public class SvgToImageSource : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            ImageSource result = null;
+
+            if (value != null && value is string src)
+            {
+                result = SvgImageSource.FromSvgResource($"XamarinAppTemplate.Images.Icons.{src}");
+            }
+
+            return result;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
