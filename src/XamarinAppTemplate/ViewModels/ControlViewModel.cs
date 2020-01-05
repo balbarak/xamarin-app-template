3097516970f39572787887f3257f285422c70ae3
balbarak <balbarak@live.com>
2020-01-05 03:15:55 +0300

    update

--- src/XamarinAppTemplate/ViewModels/ControlViewModel.cs
+++ src/XamarinAppTemplate/ViewModels/ControlViewModel.cs
@@ -1,11 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace XamarinAppTemplate.ViewModels
 {
     public class ControlViewModel : BaseViewModel
     {
+        public override Task InitializeAsync(object navigationData)
+        {
+            Title = Resx.AppResource.Controls;
 
+            return base.InitializeAsync(navigationData);
+        }
     }
 }
